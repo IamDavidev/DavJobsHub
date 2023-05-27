13c43408a8db5198aa4faca8473387b58a33d688
IamDavidev <davidlez2mgk@gmail.com>
2023-05-27 17:09:55 -0600

    [ ` update assets / images · 🍱 ` ] add icons

---
+++ src/routes/components/icons/BlobIcon.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let className;
+</script>
+
+
+<svg width="755" height="977" class={className} viewBox="0 0 755 977" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<g filter="url(#filter0_f_115_324)">
+		<ellipse cx="227.662" cy="451.378" rx="245" ry="247" transform="rotate(-108.015 227.662 451.378)" fill="url(#paint0_linear_115_324)"/>
+	</g>
+	<defs>
+		<filter id="filter0_f_115_324" x="-299.214" y="-73.8802" width="1053.75" height="1050.52" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+			<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+			<feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+			<feGaussianBlur stdDeviation="140" result="effect1_foregroundBlur_115_324"/>
+		</filter>
+		<linearGradient id="paint0_linear_115_324" x1="286.431" y1="578.886" x2="126.98" y2="327.968" gradientUnits="userSpaceOnUse">
+			<stop stop-color="#040F24" stop-opacity="0"/>
+			<stop offset="0.443258" stop-color="#461F48" stop-opacity="0.79"/>
+			<stop offset="0.755208" stop-color="#461F48"/>
+		</linearGradient>
+	</defs>
+</svg>
---
+++ src/routes/components/icons/CircleIcon.svelte
@@ -0,0 +1,9 @@
+<script lang='ts'>
+	export let className : string;
+</script>
+
+<svg class={className}  viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<circle cx="9" cy="9" r="8.5" stroke="#a6b2ec"/>
+	<circle cx="9" cy="9" r="2" fill="#a6b2ec"/>
+	<path d="M5 1.5L9 9" stroke="#a6b2ec"/>
+</svg>

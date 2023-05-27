deab1c5f8a03b7252737a991893c9fbc8e229104
IamDavidev <davidlez2mgk@gmail.com>
2023-05-27 13:30:01 -0600

    [ ` Update UI / styles / design · 💄 ` ] add colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
-      
+    fontFamily: {
+      "inter": ["Inter", "sans-serif"],
+    }   
     },
     colors: {
       "black": "#191919",
@@ -11,6 +13,7 @@ export default {
       "white" : "#F1F6F9",
       "cream": "#F7E2E2",
       "green": "#B9EDDD",
+      "blue": "#508dff"
     }
   },
   plugins: [],

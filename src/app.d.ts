328a805ec70097ccb9e94e15cd0821299b2c0f09
IamDavidev <davidlez2mgk@gmail.com>
2023-05-30 10:42:49 -0600

    [ ` Provided deploy · 🚀 ` ] fix deploy provider

--- src/app.d.ts
+++ src/app.d.ts
@@ -5,13 +5,13 @@ declare global {
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
-		interface Platform {
-			env: {
-				INFOJOBS_TOKEN: string
-				TOKEN_GIT: string
-				OPENIA_API_KEY: string
-			}
-		}
+		// interface Platform {
+		// 	env: {
+		// 		INFOJOBS_TOKEN: KVnamespace
+		// 		TOKEN_GIT: KVnamespace
+		// 		OPENIA_API_KEY: KVnamespace
+		// 	}
+		// }
 	}
 }
 

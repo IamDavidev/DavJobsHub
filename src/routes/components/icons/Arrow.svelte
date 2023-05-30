b4fb24d2a51ad11769ef60d5915d4882da6400ee
IamDavidev <davidlez2mgk@gmail.com>
2023-05-29 22:34:33 -0600

    [ ` update assets / images · 🍱 ` ] upload icons

---
+++ src/routes/components/icons/Arrow.svelte
@@ -0,0 +1,7 @@
+<script>
+	export let className = "w-6 h-6"
+</script>
+
+<svg class={className} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path d="M5 12H19M19 12L12 5M19 12L12 19" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
